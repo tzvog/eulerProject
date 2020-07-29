715d99c676f038eda3bbc34d71619fe2fe4a4e8e
tzvi <timothy.vogel@mail.huji.ac.il>
2020-07-29 19:57:55 +0300

    Initial commit

---
+++ problem1.py
@@ -0,0 +1,12 @@
+def multipleSum():
+
+    totalsum = 0
+    for i in range(1000):
+        if (i % 5) == 0 or (i % 5) == 0:
+            totalsum += i
+
+    print(totalsum)
+
+
+if __name__ == '__main__':
+    multipleSum()
